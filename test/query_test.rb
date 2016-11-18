2fdb9a6009edb0a8c3b21cb0a1dcd5dcf29af10b
Andrew Kane <acekane1@gmail.com>
2016-11-17 20:27:47 -0800

    Default to BM25

--- test/query_test.rb
+++ test/query_test.rb
@@ -12,7 +12,7 @@ class QueryTest < Minitest::Test
   end
 
   def test_with_effective_min_score
-    min_score = elasticsearch_below50? ? 0.1 : 1
+    min_score = 1
     store_names ["Milk", "Milk2"]
     assert_search "milk", ["Milk"], body_options: {min_score: min_score}
   end
